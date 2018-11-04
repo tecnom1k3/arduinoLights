e477f7891cd1362c7d6af3a738e6ac3daa4f5100
Miguel Ibarra <miguel@digitec.ws>
2018-11-04 21:51:42 +0000

    adding more routines

--- bedAndreaAttiny85.ino
+++ bedAndreaAttiny85.ino
@@ -5,7 +5,7 @@
 #define PIN 3              // pin on which the NeoPixel will be connected
 #define NUMPIXELS 3        // number of pixels in the NeoPixel
 #define BUTTON_PIN   0     // pin on which the toggle button is connected
-#define TOTAL_ROUTINES 3   // number of routines to show
+#define TOTAL_ROUTINES 8   // number of routines to show
 #define DEBOUNCE_DELAY 50  // milliseconds to debounce the toggle button
 #define LED 4              // pin on which the power on led is connected
 
@@ -57,6 +57,21 @@ void startShow(int i) {
       break;
     case 2:
       turnBlue();
+      break;
+    case 3:
+      doWipe();
+      break;
+    case 4:
+      doTheaterChase();
+      break;
+    case 5:
+      rainbow(20);
+      break;
+    case 6:
+      rainbowCycle(20);
+      break;
+    case 7:
+      theaterChaseRainbow(50);
       break;
   }
 }
@@ -156,4 +171,127 @@ ISR(INT0_vect)
     clean();
     stepNo++;
   }
+}
+
+void doWipe()
+{
+  colorWipe(strip.Color(255, 0, 0), 50); // Red
+  if (isInterrupted == HIGH) return;
+  colorWipe(strip.Color(0, 255, 0), 50); // Green
+  if (isInterrupted == HIGH) return;
+  colorWipe(strip.Color(0, 0, 255), 50); // Blue
+  if (isInterrupted == HIGH) return;
+}
+
+void colorWipe(uint32_t c, uint8_t wait)
+{
+  for (uint16_t i = 0; i < strip.numPixels(); i++) {
+    if (isInterrupted == HIGH) return;
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+void doTheaterChase()
+{
+  theaterChase(strip.Color(127, 127, 127), 50); // White
+  if (isInterrupted == HIGH) return;
+  theaterChase(strip.Color(127, 0, 0), 50); // Red
+  if (isInterrupted == HIGH) return;
+  theaterChase(strip.Color(0, 0, 127), 50); // Blue
+  if (isInterrupted == HIGH) return;
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait)
+{
+  for (int j = 0; j < 10; j++) { //do 10 cycles of chasing
+    if (isInterrupted == HIGH) return;
+    for (int q = 0; q < 3; q++) {
+      if (isInterrupted == HIGH) return;
+      for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+        if (isInterrupted == HIGH) return;
+        strip.setPixelColor(i + q, c);  //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+        if (isInterrupted == HIGH) return;
+        strip.setPixelColor(i + q, 0);      //turn every third pixel off
+      }
+    }
+  }
+}
+
+void rainbow(uint8_t wait)
+{
+  uint16_t i, j;
+
+  for (j = 0; j < 256; j++) {
+    if (isInterrupted == HIGH) return;
+    for (i = 0; i < strip.numPixels(); i++) {
+      if (isInterrupted == HIGH) return;
+      strip.setPixelColor(i, Wheel((i + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait)
+{
+  uint16_t i, j;
+
+  for (j = 0; j < 256 * 5; j++) { // 5 cycles of all colors on wheel
+    if (isInterrupted == HIGH) return;
+    for (i = 0; i < strip.numPixels(); i++) {
+      if (isInterrupted == HIGH) return;
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait)
+{
+  for (int j = 0; j < 256; j++) {   // cycle all 256 colors in the wheel
+    if (isInterrupted == HIGH) return;
+    for (int q = 0; q < 3; q++) {
+      if (isInterrupted == HIGH) return;
+      for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+        if (isInterrupted == HIGH) return;
+        strip.setPixelColor(i + q, Wheel( (i + j) % 255)); //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+        if (isInterrupted == HIGH) return;
+        strip.setPixelColor(i + q, 0);      //turn every third pixel off
+      }
+    }
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos)
+{
+  WheelPos = 255 - WheelPos;
+  if (WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if (WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
